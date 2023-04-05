deb0bee5666f77a67f7fb683f7f9e257579a01de
Satadal Banerjee <satadalbanerjeebiz@gmail.com>
2023-04-05 22:58:57 +0530

    implementation

--- FirstReactApp/textutils/src/components/TextForm.js
+++ FirstReactApp/textutils/src/components/TextForm.js
@@ -2,14 +2,23 @@ import React,{useState} from 'react'
 
 export default function TextForm(props) {
     const [text, setText] = useState("Enter Text Here");
+    
+    const handleClick = () =>{
+        let newText = text.toUpperCase();
+        setText(newText);
+    }
 
-    return (
+    const handleOnChange = (event) =>{
+        setText(event.target.value);
+    }
+
+    return ( 
     <div>
         <h1>{props.heading}</h1>
         <div class="mb-3">
-        <textarea class="form-control" id="exampleFormControlTextarea1" value={text} rows="8"></textarea>
+        <textarea class="form-control" id="exampleFormControlTextarea1" value={text} onChange={handleOnChange} rows="8"></textarea>
         </div>  
-        <button className="btn btn-primary">Convert TO UpperCase</button>
+        <button className="btn btn-primary" onClick={handleClick}>Convert TO UpperCase</button>
     </div>
   )
 }
